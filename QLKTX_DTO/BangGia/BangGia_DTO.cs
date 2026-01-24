2b827b952d7e2aaf8f92e8e79e25bfbb3b4502af
Tâm Trương <truongthanhtam2125@gmail.com>
2026-01-24 11:19:26 +0700

    fix bg

--- QLKTX_DTO/BangGia/BangGia_DTO.cs
+++ QLKTX_DTO/BangGia/BangGia_DTO.cs
@@ -1,11 +1,15 @@
-public class BangGia_DTO
+namespace QLKTX_DTO.BangGia
 {
-    public int MaBangGia { get; set; }
-    public string LoaiPhong { get; set; } = null!;
-    public decimal DonGiaPhong { get; set; }
-    public decimal DonGiaDien { get; set; }
-    public decimal DonGiaNuoc { get; set; }
-    public decimal PhiRac { get; set; }     // 👈 thêm
-    public DateTime NgayApDung { get; set; }
-    public bool DangSuDung { get; set; }
+    public class BangGia_DTO
+    {
+        public int MaBangGia { get; set; }
+        public string LoaiPhong { get; set; } = null!;
+        public decimal DonGiaPhong { get; set; }
+        public decimal DonGiaDien { get; set; }
+        public decimal DonGiaNuoc { get; set; }
+        public decimal PhiRac { get; set; }     // 👈 thêm
+        public DateTime NgayApDung { get; set; }
+        public bool DangSuDung { get; set; }
+    }
 }
+
--- QLKTX_DTO/BangGia/CreateBangGia_DTO.cs
+++ QLKTX_DTO/BangGia/CreateBangGia_DTO.cs
@@ -1,9 +1,14 @@
-public class CreateBangGia_DTO
+namespace QLKTX_DTO.BangGia
 {
-    public string LoaiPhong { get; set; } = null!;
-    public decimal DonGiaPhong { get; set; }
-    public decimal DonGiaDien { get; set; }
-    public decimal DonGiaNuoc { get; set; }
-    public decimal PhiRac { get; set; }     // 👈 thêm
-    public DateTime? NgayApDung { get; set; } // optional
+    public class CreateBangGia_DTO
+    {
+
+        public string LoaiPhong { get; set; } = null!;
+        public decimal DonGiaPhong { get; set; }
+        public decimal DonGiaDien { get; set; }
+        public decimal DonGiaNuoc { get; set; }
+        public decimal PhiRac { get; set; }     // 👈 thêm
+        public DateTime? NgayApDung { get; set; } // optional
+    }
 }
+
